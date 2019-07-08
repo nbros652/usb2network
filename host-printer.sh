bca906d65624e23e7498bac79b27d9d015801d17
nbros652 <nbros652@users.noreply.github.com>
2019-07-08 15:08:37 +0800

    added support for >1 USB port

--- host-printer.sh
+++ host-printer.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+modprobe usbip-core
+modprobe usbip-host
+systemctl stop cups
 pidof usbipd || usbipd -D
-usbip bind -b 1-1
-
+# vendor:product ids of desired printer to attach 
+# (uncomment and update the next line if you're using a USB hub or rpi with multiple USB ports)
+#ids=0000:0000
+if [ -z "$ids" ]; then
+  dev=1-1
+else
+  dev="$(usbip list -l | grep $ids | grep -oP "\d-\d\.\d")"
+fi
+usbip bind -b $dev
+systemctl start cups
