50e43cbdf6725da3ff4a751d6de1aa95721b97ca
nbros652 <nbros652@users.noreply.github.com>
2019-07-08 15:11:51 +0800

    added support for >1 USB port

--- stop-hosting-printer.sh
+++ stop-hosting-printer.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-usbip unbind -b 1-1
+systemctl stop cups
+
+# vendor:product ids of desired printer to attach 
+# (uncomment and update the next line if you're using a USB hub or rpi with multiple USB ports)
+#ids=0000:0000
+if [ -z "$ids" ]; then
+  dev=1-1
+else
+  dev="$(usbip list -l | grep $ids | grep -oP "\d-\d\.\d")"
+fi
+
+usbip unbind -b $dev
+
+systemctl start cups
